3d8e5591f83a6b108a135674a2bf6802447ef940
inamuu <8310973+inamuu@users.noreply.github.com>
2019-09-05 18:41:57 +0900

    :+1: sslproxiesから接続情報を取得する

---
+++ proxy.py
@@ -0,0 +1,26 @@
+# coding: utf-8
+
+import re
+import requests
+from bs4 import BeautifulSoup
+
+class GetProxyList:
+    def parse_sslproxies(self):
+        try:
+            r = requests.get("https://www.sslproxies.org")
+            soup = BeautifulSoup(r.content, "html.parser")
+            tbody = soup.find('tbody').find_all('td')
+        except Exception as e:
+            print(e)
+        else:
+            for rows in tbody:
+                rtext = rows.string
+                ipaddr = re.match('(([1-9]?[0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([1-9]?[0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])', rtext)
+                if ipaddr is not None:
+                    print('ipaddr: %s' % format(ipaddr.group(0)))
+                port = re.match('\d{4,5}', rtext)
+                if port is not None:
+                    print('port: %s' % format(port.group(0)))
+
+proxy = GetProxyList()
+proxy.parse_sslproxies()
