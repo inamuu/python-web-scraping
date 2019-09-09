4c50225f6aba88ecc2be4d2204c11269fd785e34
inamuu <8310973+inamuu@users.noreply.github.com>
2019-09-09 12:52:21 +0900

    :+1: 多次元配列にする

--- getprl/GetProxyList.py
+++ getprl/GetProxyList.py
@@ -10,21 +10,12 @@ class GetProxyList:
         try:
             r = requests.get("https://www.sslproxies.org")
             soup = BeautifulSoup(r.content, "html.parser")
-            tbody = soup.find('tbody').find_all('td')
+            tbody = soup.find('tbody').findAll('tr')
         except Exception as e:
             print(e)
         else:
+            proxylist = [] 
             for rows in tbody:
-                rtext = rows.string
-                ipaddr = re.match('(([1-9]?[0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([1-9]?[0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])', rtext)
-
-                if ipaddr is not None:
-                    print('proxy_count: %s' % proxy_count)
-                    print('ipaddr: %s' % format(ipaddr.group(0)))
-                    proxy_count = proxy_count + 1
-                port = re.match('\d{4,5}', rtext)
-                if port is not None:
-                    print('port: %s' % format(port.group(0)))
-
-#proxy = GetProxyList()
-#proxy.parse_sslproxies()
+                ipaddr = rows.find('td').string
+                port = rows.td.next_sibling.string
+                proxylist.append([ipaddr, port])
