ffaf3b07525b225209c2ee8c268ac05bceb16394
inamuu <8310973+inamuu@users.noreply.github.com>
2019-09-09 10:29:28 +0900

    :+1: clickでサブコマンド実装

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 # coding: utf-8
 
+import click
 import sys
 from getprl import GetProxyList as getprl
 
-def main():
+@click.group()
+def subcommand():
+    pass
+
+@subcommand.command(help='プロキシ一覧を取得します')
+def get_proxy_list():
     proxy = getprl.GetProxyList()
     proxy.parse_sslproxies()
 
